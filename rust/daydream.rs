31f00da8845048d170717aaec8c8fdf30144e9fd
RyoGreen <ryo.nohara@casting-one.jp>
2024-05-14 23:21:09 +0900

    daydream

---
+++ rust/daydream.rs
@@ -0,0 +1,32 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        s: String,
+    };
+    let keywords = vec!["dream", "dreamer", "erase", "eraser"];
+    for i in 0..keywords.len() {
+        if s == keywords[i] {
+            println!("YES");
+            return;
+        }
+        for j in i..keywords.len() {
+            if s == keywords[i].to_string() + keywords[j] {
+                println!("YES");
+                return;
+            }
+            for k in j..keywords.len() {
+                if s == keywords[i].to_string() + keywords[j] + keywords[k] {
+                    println!("YES");
+                    return;
+                }
+                for l in k..keywords.len() {
+                    if s == keywords[i].to_string() + keywords[j] + keywords[k] + keywords[l] {
+                        println!("YES");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
