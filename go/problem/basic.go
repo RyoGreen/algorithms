867197613ee11a0235436a8aa263c36c9a1de308
RyoGreen <41415120ryo@gmail.com>
2024-06-08 21:07:12 +0900

    sort

---
+++ go/problem/basic.go
@@ -0,0 +1,52 @@
+package problem
+
+func is_prime(n int) bool {
+
+	if n < 2 {
+		return false
+	}
+	if n == 2 || n == 3 {
+		return true
+	}
+
+	if n%2 == 0 || n%3 == 0 {
+		return false
+	}
+
+	for i := 5; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func quickSort(n []int) []int {
+	if len(n) < 2 {
+		return n
+	}
+
+	index := len(n) / 2
+
+	p := n[index]
+	var right, left []int
+	for i, v := range n {
+		if i == index {
+			continue
+		}
+		if v >= p {
+			right = append(right, v)
+		} else {
+			left = append(left, v)
+		}
+	}
+
+	right = quickSort(right)
+	left = quickSort(left)
+
+	var result []int
+	result = append(left, p)
+	result = append(result, right...)
+
+	return result
+}
