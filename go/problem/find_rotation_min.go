d979212973848eee85cf0d4ccad5aabc090420f7
RyoGreen <41415120ryo@gmail.com>
2024-06-15 17:22:53 +0900

    find rotation min

---
+++ go/problem/find_rotation_min.go
@@ -0,0 +1,15 @@
+package problem
+
+func FindRotationMin(nums []int) int {
+	var left, right = 0, len(nums) - 1
+	for left < right {
+		mid := (left + right) / 2
+		if nums[mid] > nums[right] {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+
+	return nums[left]
+}
