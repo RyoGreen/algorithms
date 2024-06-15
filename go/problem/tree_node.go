fe11b448b13d4753fcce8b954ac020b3f6e86086
RyoGreen <41415120ryo@gmail.com>
2024-06-16 00:00:15 +0900

    node tree

---
+++ go/problem/tree_node.go
@@ -0,0 +1,54 @@
+package problem
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type BST struct {
+	Root *TreeNode
+}
+
+func (bst *BST) insert(val int) {
+	n := &TreeNode{Val: val}
+	if bst.Root == nil {
+		bst.Root = n
+		return
+	}
+	root := bst.Root
+	for {
+		if val < root.Val {
+			if root.Left == nil {
+				root.Left = &TreeNode{Val: val}
+				return
+			}
+			root = root.Left
+		} else {
+			if root.Right == nil {
+				root.Right = &TreeNode{Val: val}
+				return
+			}
+			root = root.Right
+		}
+	}
+}
+
+func (bst *BST) search(val int) bool {
+	current := bst.Root
+	for {
+		if current == nil {
+			return false
+		}
+		if current.Val == val {
+			return true
+		}
+
+		if current.Val > val {
+			current = current.Left
+		} else {
+			current = current.Right
+		}
+
+	}
+}
