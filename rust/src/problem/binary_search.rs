4e52627292fec6884803bf7c357692fa549ea573
RyoGreen <ryo.nohara@casting-one.jp>
2024-06-16 16:48:48 +0900

    find first occurrence

--- rust/src/problem/binary_search.rs
+++ rust/src/problem/binary_search.rs
@@ -16,3 +16,24 @@ pub fn binary_search(arr: &[i32], target: i32) -> i32 {
 
     -1
 }
+
+pub fn find_first_occurrence(arr: &[i32], target: i32) -> isize {
+    let mut left = 0;
+    let mut right = arr.len() as isize - 1;
+    let mut result = -1;
+
+    while left <= right {
+        let mid = left + (right - left) / 2;
+        if arr[mid as usize] == target {
+            result = mid;
+            right = mid - 1;
+        }
+        if arr[mid as usize] < target {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    result
+}
