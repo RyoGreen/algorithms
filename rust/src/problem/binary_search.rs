c2aff30121e8fd1e83cba0e0cea9c9f9f00ca378
RyoGreen <ryo.nohara@casting-one.jp>
2024-06-16 23:00:21 +0900

    find_range

--- rust/src/problem/binary_search.rs
+++ rust/src/problem/binary_search.rs
@@ -37,3 +37,31 @@ pub fn find_first_occurrence(arr: &[i32], target: i32) -> isize {
 
     result
 }
+
+pub fn search_range(arr: &[i32], target: i32) -> Vec<i32> {
+    let first = find_first_occurrence(arr, target);
+    let last = find_last_occurrence(arr, target);
+
+    vec![first as i32, last as i32]
+}
+
+fn find_last_occurrence(arr: &[i32], target: i32) -> isize {
+    let mut left = 0;
+    let mut right = arr.len() as isize - 1;
+    let mut result = -1;
+
+    while left <= right {
+        let mid = left + (right - left) / 2;
+        if arr[mid as usize] == target {
+            result = mid;
+            left = mid + 1;
+        }
+        if arr[mid as usize] < target {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    result
+}
