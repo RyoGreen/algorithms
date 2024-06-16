84351ebb825cb9212fb4c60a363a09be54b4b7ea
RyoGreen <ryo.nohara@casting-one.jp>
2024-06-16 15:36:39 +0900

    binary search

--- rust/src/problem/binary_search.rs
+++ rust/src/problem/binary_search.rs
@@ -1,8 +1,8 @@
-pub fn binary_search_last(arr: &[i32], target: i32) -> i32 {
+pub fn binary_search(arr: &[i32], target: i32) -> i32 {
     let mut left = 0;
-    let mut right = arr.len();
+    let mut right = arr.len() - 1;
 
-    while left < right {
+    while left <= right {
         let mid = left + (right - left) / 2;
         if arr[mid] == target {
             return mid as i32;
@@ -10,7 +10,7 @@ pub fn binary_search_last(arr: &[i32], target: i32) -> i32 {
         if arr[mid] < target {
             left = mid + 1;
         } else {
-            right = mid;
+            right = mid - 1;
         }
     }
 
