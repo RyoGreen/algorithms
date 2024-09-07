208e94e8610f3b11ab1ad2ab731402f4c23526ad
RyoGreen <ryo.nohara@casting-one.jp>
2024-09-07 20:25:09 +0900

    merge interval

--- go/problem/merge_interval.go
+++ go/problem/merge_interval.go
@@ -1,6 +1,32 @@
 package problem
 
-func MergeInterval(intervals [][]int) [][]int {
+import "sort"
 
-	return [][]int{}
+func MergeInterval(intervals [][]int) [][]int {
+	if len(intervals) == 0 {
+		return [][]int{}
+	}
+
+	// Sort the intervals by start time
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	merged := [][]int{intervals[0]}
+	for i := 1; i < len(intervals); i++ {
+		if merged[len(merged)-1][1] >= intervals[i][0] {
+			merged[len(merged)-1][1] = max(merged[len(merged)-1][1], intervals[i][1])
+		} else {
+			merged = append(merged, intervals[i])
+		}
+	}
+
+	return merged
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
