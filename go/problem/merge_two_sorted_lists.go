6775dc3f6d9172a36b8f61a281a3ea35b7eebe2a
RyoGreen <41415120ryo@gmail.com>
2024-09-12 20:06:44 +0900

    save

--- go/problem/merge_two_sorted_lists.go
+++ go/problem/merge_two_sorted_lists.go
@@ -6,6 +6,21 @@ type ListNode struct {
 }
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	s := []int{}
+	for {
+		s = append(s, list1.Val)
+		if list1.Next == nil {
+			break
+		}
+	}
+	s2 := []int
+	for {
+		s = append(s, list1.Val)
+		if list1.Next == nil {
+			break
+		}
+
+	}
 
 	return list1
 }
