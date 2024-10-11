fb6fec773c5550d138ca2e1f355462bc268c213c
RyoGreen <41415120ryo@gmail.com>
2024-10-11 00:02:44 -0400

    get min time

--- go/past_interview/get_min_time.go
+++ go/past_interview/get_min_time.go
@@ -1,3 +1,17 @@
 package pastinterview
 
-func GetMinTime(n int, request string, mingap int) int {}
+func GetMinTime(n int, request string, mingap int) int {
+	lastProcessed := make(map[rune]int)
+	time := 0
+
+	for _, r := range request {
+		if lastTime, ok := lastProcessed[r]; ok {
+			if time-lastTime < mingap {
+				time = lastTime + mingap
+			}
+		}
+		lastProcessed[r] = time
+		time++
+	}
+	return time
+}
