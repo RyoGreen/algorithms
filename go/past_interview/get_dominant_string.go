7179150b30487d3b011432125e203f9dc5a13a1d
RyoGreen <41415120ryo@gmail.com>
2024-10-18 01:56:12 -0400

    past interview

--- go/past_interview/get_dominant_string.go
+++ go/past_interview/get_dominant_string.go
@@ -1,5 +1,31 @@
 package pastinterview
 
-func GetDominantString(s string) int {
+func getDominantStringCount(s string) int {
+	n := len(s)
+	dominantCount := 0
 
+	for length := 2; length <= n; length += 2 {
+		charCount := make([]int, 25)
+
+		for start := 0; start <= n-length; start++ {
+			if start == 0 {
+				for i := 0; i < length; i++ {
+					charCount[s[start+i]-'a']++
+				}
+			} else {
+				charCount[s[start-1]-'a']--
+				charCount[s[start+length-1]-'a']++
+			}
+
+			targetFrequency := length / 2
+			for _, count := range charCount {
+				if count == targetFrequency {
+					dominantCount++
+					break
+				}
+			}
+		}
+	}
+
+	return dominantCount
 }
