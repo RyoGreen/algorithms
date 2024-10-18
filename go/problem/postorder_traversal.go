2afb35e975e78cedea82f2e67d3eef6140b3d266
RyoGreen <41415120ryo@gmail.com>
2024-10-18 01:55:42 -0400

    Binary Tree Postorder Traversal

---
+++ go/problem/postorder_traversal.go
@@ -0,0 +1,14 @@
+package problem
+
+func postorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	var result []int
+	result = append(result, postorderTraversal(root.Left)...)
+	result = append(result, postorderTraversal(root.Right)...)
+	result = append(result, root.Val)
+
+	return result
+}
