01f860ce07a0718526938b22ae1deb25769b4b44
RyoGreen <41415120ryo@gmail.com>
2024-10-21 20:15:01 -0400

    reverse int

--- go/problem/reverse_string.go
+++ go/problem/reverse_string.go
@@ -1,5 +1,10 @@
 package problem
 
+import (
+	"math"
+	"strconv"
+)
+
 func ReverseString(s string) string {
 	r := []rune(s)
 	length := len(r) / 2
@@ -7,4 +12,26 @@ func ReverseString(s string) string {
 		r[i], r[len(r)-i-1] = r[len(r)-i-1], r[i]
 	}
 	return string(r)
+}
+
+func ReverseInt(x int) int {
+	var (
+		s      string
+		isPlus bool
+	)
+	s = strconv.Itoa(int(math.Abs(float64(x))))
+	if x > 0 {
+		isPlus = true
+	}
+	r := []rune(s)
+	length := len(r) / 2
+	for i := 0; i < length; i++ {
+		r[i], r[len(r)-i-1] = r[len(r)-i-1], r[i]
+	}
+
+	result, _ := strconv.Atoi(string(r))
+	if !isPlus {
+		return -result
+	}
+	return result
 }
