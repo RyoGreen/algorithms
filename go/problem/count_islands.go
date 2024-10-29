5f5fad20e6ef89431dc52d013712e207ab40e632
RyoGreen <41415120ryo@gmail.com>
2024-10-29 16:09:06 -0400

    count islands

---
+++ go/problem/count_islands.go
@@ -0,0 +1,6 @@
+package problem
+
+func countIslands(grid [][]int) int {
+
+	return 0
+}
---
+++ go/problem/count_islands_test.go
@@ -0,0 +1,57 @@
+package problem
+
+import "testing"
+
+func TestCountIslands(t *testing.T) {
+	tests := []struct {
+		name string
+		grid [][]int
+		want int
+	}{
+		{
+			name: "No islands",
+			grid: [][]int{
+				{0, 0, 0},
+				{0, 0, 0},
+				{0, 0, 0},
+			},
+			want: 0,
+		},
+		{
+			name: "One island",
+			grid: [][]int{
+				{1, 1, 0},
+				{1, 0, 0},
+				{0, 0, 0},
+			},
+			want: 1,
+		},
+		{
+			name: "Multiple islands",
+			grid: [][]int{
+				{1, 0, 0},
+				{0, 1, 1},
+				{0, 0, 1},
+			},
+			want: 2,
+		},
+		{
+			name: "All land",
+			grid: [][]int{
+				{1, 1, 1},
+				{1, 1, 1},
+				{1, 1, 1},
+			},
+			want: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := countIslands(tt.grid)
+			if got != tt.want {
+				t.Errorf("CountIslands() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
