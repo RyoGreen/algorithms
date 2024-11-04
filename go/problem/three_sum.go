e4fd2dff0b8772231308652f0dcd50f83f55e857
RyoGreen <41415120ryo@gmail.com>
2024-11-04 00:19:15 -0500

    three sum closest

--- go/problem/three_sum.go
+++ go/problem/three_sum.go
@@ -1,6 +1,9 @@
 package problem
 
-import "sort"
+import (
+	"math"
+	"sort"
+)
 
 func threeSum(nums []int) [][]int {
 	var result [][]int
@@ -30,6 +33,34 @@ func threeSum(nums []int) [][]int {
 				r--
 			default:
 				l++
+			}
+		}
+	}
+
+	return result
+}
+
+func threeSumClosest(nums []int, target int) int {
+	var result = nums[0] + nums[1] + nums[2]
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		l, r := i+1, len(nums)-1
+		for l < r {
+			sum := nums[i] + nums[l] + nums[r]
+
+			if math.Abs(float64(target-sum)) < math.Abs(float64(target-result)) {
+				result = sum
+			}
+
+			if sum == target {
+				return sum
+			} else if sum < target {
+				l++
+			} else {
+				r--
 			}
 		}
 	}
