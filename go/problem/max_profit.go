9a1f550cd6bd69e35797f33c75a49160c2f144f1
RyoGreen <41415120ryo@gmail.com>
2024-11-09 00:29:38 -0500

    max sub array

--- go/problem/max_profit.go
+++ go/problem/max_profit.go
@@ -25,3 +25,24 @@ func maxProfit(prices []int) int {
 
 	return result
 }
+
+/*
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+*/
+func maxSubArray(nums []int) int {
+	var (
+		maxSum = nums[0]
+		curSum = 0
+	)
+
+	for _, v := range nums {
+		if curSum < 0 {
+			curSum = 0
+		}
+		curSum += v
+		maxSum = max(maxSum, curSum)
+	}
+
+	return maxSum
+}
