8ea2b5e74d2bcfba5d9bc6ed76846d8fa3bb0741
RyoGreen <41415120ryo@gmail.com>
2024-11-15 00:25:57 -0500

    dp test

--- go/problem/dp_test.go
+++ go/problem/dp_test.go
@@ -1,1 +1,28 @@
 package problem
+
+import "testing"
+
+func TestFibonacci(t *testing.T) {
+	// テストケースの定義
+	tests := []struct {
+		input    int
+		expected int
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 1},
+		{3, 2},
+		{4, 3},
+		{5, 5},
+		{6, 8},
+		{10, 55},
+		{20, 6765},
+	}
+	for _, test := range tests {
+		result := fibonacci(test.input)
+		if result != test.expected {
+			t.Errorf("fibonacci(%d) = %d; want %d", test.input, result, test.expected)
+		}
+
+	}
+}
