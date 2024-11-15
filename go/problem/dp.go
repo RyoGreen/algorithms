acfdfaa0b4c1f2c6a1104ca6d03767e592cd702c
RyoGreen <41415120ryo@gmail.com>
2024-11-14 23:14:21 -0500

    clim stairs

--- go/problem/dp.go
+++ go/problem/dp.go
@@ -1,1 +1,14 @@
 package problem
+
+func climbStairs(n int) int {
+	if n <= 2 {
+		return n
+	}
+
+	first, second := 1, 2
+	for i := 3; i <= n; i++ {
+		first, second = second, first+second
+	}
+
+	return second
+}
