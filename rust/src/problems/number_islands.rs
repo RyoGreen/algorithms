6b47d1dfaf87d69f4087feaba56f4978c4072909
RyoGreen <41415120ryo@gmail.com>
2024-12-07 13:11:35 -0500

    fix argment

--- rust/src/problems/number_islands.rs
+++ rust/src/problems/number_islands.rs
@@ -1,22 +1,34 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn num_islands(grid: Vec<Vec<char>>) -> i32 {
+    pub fn num_islands(mut grid: Vec<Vec<char>>) -> i32 {
         if grid.is_empty() {
             return 0;
         }
-        let mut grid = grid;
+
         let rows = grid.len();
         let cols = grid[0].len();
         let mut result: i32 = 0;
+
         fn dfs(grid: &mut Vec<Vec<char>>, i: usize, j: usize, rows: usize, cols: usize) {
             if i >= rows || j >= cols || grid[i][j] == '0' {
                 return;
             }
-            dfs(grid, i - 1, j, rows, cols);
-            dfs(grid, i + 1, j, rows, cols);
-            dfs(grid, i, j - 1, rows, cols);
-            dfs(grid, i, j + 1, rows, cols);
+
+            grid[i][j] = '0';
+
+            if i > 0 {
+                dfs(grid, i - 1, j, rows, cols);
+            }
+            if i + 1 < rows {
+                dfs(grid, i + 1, j, rows, cols);
+            }
+            if j > 0 {
+                dfs(grid, i, j - 1, rows, cols);
+            }
+            if j + 1 < cols {
+                dfs(grid, i, j + 1, rows, cols);
+            }
         }
 
         for i in 0..rows {
@@ -27,6 +39,7 @@ impl Solution {
                 }
             }
         }
+
         result
     }
 }
