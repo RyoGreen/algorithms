1e2aaa69a0504158f3344a90293d3d96db2bfb28
RyoGreen <41415120ryo@gmail.com>
2024-12-06 23:36:22 -0500

    function in the function

--- rust/src/problems/number_islands.rs
+++ rust/src/problems/number_islands.rs
@@ -2,6 +2,12 @@ pub struct Solution;
 
 impl Solution {
     pub fn num_islands(grid: Vec<Vec<char>>) -> i32 {
+        fn dfs(i: i32, j: i32) {
+            dfs(i + 1, j);
+            dfs(i - 1, j);
+            dfs(i, j + 1);
+            dfs(i, j - 1);
+        }
         for i in grid {
             for j in i {}
         }
