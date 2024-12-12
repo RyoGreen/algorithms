fc66c684d001a25e47de7eb0fee69c71c8c995fe
RyoGreen <41415120ryo@gmail.com>
2024-12-11 21:55:47 -0500

    contain duplicate

--- rust/src/problems/easy_challenge.rs
+++ rust/src/problems/easy_challenge.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 pub struct Solution;
 
 impl Solution {
@@ -32,5 +34,24 @@ impl Solution {
         nums.reverse();
         nums[0..k].reverse();
         nums[k..].reverse();
+    }
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut map = HashMap::new();
+        for n in nums {
+            if map.entry(n).or_insert(()) == &() {
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn contains_duplicate_v2(nums: Vec<i32>) -> bool {
+        let mut set = HashSet::new();
+        for n in nums {
+            if !set.insert(n) {
+                return true;
+            }
+        }
+        false
     }
 }
