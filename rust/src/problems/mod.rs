5b2c7fb749a2f385019a54b9790987658dad29a1
RyoGreen <41415120ryo@gmail.com>
2025-03-10 19:26:19 +0900

    fix test

--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -2,7 +2,6 @@ pub mod car_fleet;
 pub mod easy_challenge;
 pub mod fib;
 pub mod length_of_longest_substring;
-pub mod longest_palindrome;
 pub mod max_depth;
 pub mod max_profit;
 pub mod number_islands;
