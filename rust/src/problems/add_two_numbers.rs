70ad5ff4d244cab72d302b166fed7cef37407c9e
RyoGreen <41415120ryo@gmail.com>
2025-07-15 23:54:13 +0900

    add two numbers

--- rust/src/problems/add_two_numbers.rs
+++ rust/src/problems/add_two_numbers.rs
@@ -6,5 +6,30 @@ impl Solution {
         l1: Option<Box<ListNode>>,
         l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
+        let mut dummy_head = ListNode::new(0);
+        let mut current = &mut dummy_head;
+        let mut carry = 0;
+
+        let mut p1 = l1.as_ref();
+        let mut p2 = l2.as_ref();
+
+        while p1.is_some() || p2.is_some() || carry > 0 {
+            let x = p1.map_or(0, |node| node.val);
+            let y = p2.map_or(0, |node| node.val);
+
+            let sum = x + y + carry;
+            carry = sum / 10;
+            current.next = Some(Box::new(ListNode::new(sum % 10)));
+            current = current.next.as_mut().unwrap();
+
+            if let Some(node) = p1 {
+                p1 = node.next.as_ref();
+            }
+            if let Some(node) = p2 {
+                p2 = node.next.as_ref();
+            }
+        }
+
+        dummy_head.next
     }
 }
