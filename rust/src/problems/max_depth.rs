3812dd454d1f4620faf32d4e757350c04b4cb4ea
RyoGreen <41415120ryo@gmail.com>
2025-07-19 00:24:49 +0900

    treenode

--- rust/src/problems/max_depth.rs
+++ rust/src/problems/max_depth.rs
@@ -1,25 +1,7 @@
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
-        }
-    }
-}
+use super::structs::TreeNode;
+use std::{cell::RefCell, cmp, rc::Rc};
 
 pub struct Solution;
-use std::cell::RefCell;
-use std::cmp;
-use std::rc::Rc;
 impl Solution {
     pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
         match root {
