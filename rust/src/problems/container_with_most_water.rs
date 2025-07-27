0f2e77501216a04be7a07874a0c109e7f81f2ad2
RyoGreen <41415120ryo@gmail.com>
2025-07-27 22:10:20 +0900

    v1

--- rust/src/problems/container_with_most_water.rs
+++ rust/src/problems/container_with_most_water.rs
@@ -1,7 +1,17 @@
+use std::cmp;
 pub struct Solution;
 
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
-        1
+        let mut area: i32 = 0;
+        for (i, v) in height.iter().enumerate() {
+            for n in i..height.len() {
+                let tmp = (n - i) as i32 * cmp::min(height[n], *v);
+                if tmp > area {
+                    area = tmp;
+                }
+            }
+        }
+        area
     }
 }
