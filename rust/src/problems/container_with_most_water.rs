9132db5aeda1787792c85e4916f02eaca157bd40
RyoGreen <41415120ryo@gmail.com>
2025-07-27 23:01:41 +0900

    v2

--- rust/src/problems/container_with_most_water.rs
+++ rust/src/problems/container_with_most_water.rs
@@ -15,9 +15,7 @@ impl Solution {
         area
     }
     pub fn max_area_v2(height: Vec<i32>) -> i32 {
-        let mut area = 0;
-        let mut left = 0;
-        let mut right = height.len() - 1;
+        let (mut area, mut left, mut right) = (0, 0, height.len() - 1);
         while right > left {
             let tmp = (right - left) as i32 * (cmp::min(height[left], height[right]));
             area = cmp::max(tmp, area);
