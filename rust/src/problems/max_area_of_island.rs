590f0d11fb5190a33c5b69f661503aee311b467e
RyoGreen <41415120ryo@gmail.com>
2025-08-23 00:08:59 +0900

    area of island

--- rust/src/problems/max_area_of_island.rs
+++ rust/src/problems/max_area_of_island.rs
@@ -9,26 +9,25 @@ impl Solution {
 
         for i in 0..m {
             for j in 0..n {
-                if grid[i][j] == 1 {
-                    let area = Self::dfs(i, j, &mut grid);
-                    max_area = max_area.max(area + 1);
-                }
+                let area = Self::dfs(i as i32, j as i32, &mut grid);
+                max_area = max_area.max(area);
             }
         }
         return max_area;
     }
-    fn dfs(i: usize, j: usize, grid: &mut Vec<Vec<i32>>) -> i32 {
-        if i >= grid.len() || j >= grid[0].len() || grid[i][j] == 0 {
+    fn dfs(i: i32, j: i32, grid: &mut Vec<Vec<i32>>) -> i32 {
+        if i >= grid.len() as i32
+            || j >= grid[0].len() as i32
+            || i < 0
+            || j < 0
+            || grid[i as usize][j as usize] == 0
+        {
             return 0;
         }
-        let mut max_area = 0;
-        if grid[i][j] == 1 {
-            grid[i][j] = 0;
-            max_area = Self::dfs(i.wrapping_sub(1), j, grid)
-                + Self::dfs(i + 1, j, grid)
-                + Self::dfs(i, j.wrapping_sub(1), grid)
-                + Self::dfs(i, j + 1, grid);
-        }
-        return max_area;
+        grid[i as usize][j as usize] = 0;
+        1 + Self::dfs(i - 1, j, grid)
+            + Self::dfs(i + 1, j, grid)
+            + Self::dfs(i, j - 1, grid)
+            + Self::dfs(i, j + 1, grid)
     }
 }
