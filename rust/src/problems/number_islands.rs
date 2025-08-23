cab527019de9dc3fa05e98ad97de5956062d54b3
RyoGreen <41415120ryo@gmail.com>
2025-08-23 15:04:09 +0900

    number of island

--- rust/src/problems/number_islands.rs
+++ rust/src/problems/number_islands.rs
@@ -5,41 +5,37 @@ impl Solution {
         if grid.is_empty() {
             return 0;
         }
-
         let rows = grid.len();
         let cols = grid[0].len();
         let mut result: i32 = 0;
-
-        fn dfs(grid: &mut Vec<Vec<char>>, i: usize, j: usize, rows: usize, cols: usize) {
-            if i >= rows || j >= cols || grid[i][j] == '0' {
-                return;
-            }
-
-            grid[i][j] = '0';
-
-            if i > 0 {
-                dfs(grid, i - 1, j, rows, cols);
-            }
-            if i + 1 < rows {
-                dfs(grid, i + 1, j, rows, cols);
-            }
-            if j > 0 {
-                dfs(grid, i, j - 1, rows, cols);
-            }
-            if j + 1 < cols {
-                dfs(grid, i, j + 1, rows, cols);
-            }
-        }
-
         for i in 0..rows {
             for j in 0..cols {
                 if grid[i][j] == '1' {
                     result += 1;
-                    dfs(&mut grid, i, j, rows, cols);
+                    Solution::dfs(&mut grid, i, j, rows, cols);
                 }
             }
         }
-
         result
+    }
+    fn dfs(grid: &mut Vec<Vec<char>>, i: usize, j: usize, rows: usize, cols: usize) {
+        if i >= rows || j >= cols || grid[i][j] == '0' {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        if i > 0 {
+            Solution::dfs(grid, i - 1, j, rows, cols);
+        }
+        if i + 1 < rows {
+            Solution::dfs(grid, i + 1, j, rows, cols);
+        }
+        if j > 0 {
+            Solution::dfs(grid, i, j - 1, rows, cols);
+        }
+        if j + 1 < cols {
+            Solution::dfs(grid, i, j + 1, rows, cols);
+        }
     }
 }
