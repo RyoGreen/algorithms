2240a3bea220de4935a2fc8ee036b31884a4ad1b
RyoGreen <41415120ryo@gmail.com>
2025-10-18 10:39:44 +0900

    add test

--- rust/tests/fib_test.rs
+++ rust/tests/fib_test.rs
@@ -8,3 +8,12 @@ fn test_fib() {
     assert_eq!(Solution::fib(5), 5);
     assert_eq!(Solution::fib(6), 8);
 }
+
+#[test]
+fn test_fib_v1() {
+    assert_eq!(Solution::fib_v1(2), 1);
+    assert_eq!(Solution::fib_v1(3), 2);
+    assert_eq!(Solution::fib_v1(4), 3);
+    assert_eq!(Solution::fib_v1(5), 5);
+    assert_eq!(Solution::fib_v1(6), 8);
+}
