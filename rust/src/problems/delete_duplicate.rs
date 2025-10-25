1442ccd2ff1d6d3552ca77763576ee62e9df860d
RyoGreen <41415120ryo@gmail.com>
2025-10-25 20:05:46 +0900

    delete duplicate

--- rust/src/problems/delete_duplicate.rs
+++ rust/src/problems/delete_duplicate.rs
@@ -1,12 +1,17 @@
 pub struct Solution;
-
 use super::structs::ListNode;
 
 impl Solution {
-    pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        let mut dummy = ListNode::new(0);
-        dummy.next = head;
+    pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut current = head.as_mut()?;
 
-        dummy.next
+        while let Some(next) = current.next.as_mut() {
+            if current.val == next.val {
+                current.next = next.next.take();
+            } else {
+                current = current.next.as_mut().unwrap();
+            }
+        }
+        head
     }
 }
