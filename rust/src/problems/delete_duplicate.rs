10ed2da6305bdfe59d2e16142a1380a24e42aa04
RyoGreen <41415120ryo@gmail.com>
2025-10-26 13:30:38 +0900

    deelte duplicate

--- rust/src/problems/delete_duplicate.rs
+++ rust/src/problems/delete_duplicate.rs
@@ -14,4 +14,33 @@ impl Solution {
         }
         head
     }
+
+    // Input: head = [1,2,3,3,3,4,4,5]
+    // Output: [1,2,5]
+    pub fn delete_duplicates_v2(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut new_head = Box::new(ListNode::new(0));
+        let mut current = &mut new_head;
+        let mut head = head;
+
+        while let Some(mut node) = head {
+            let mut is_duplicate = false;
+
+            while let Some(next_node) = &node.next {
+                if node.val == next_node.val {
+                    is_duplicate = true;
+                    node.next = node.next.take().unwrap().next;
+                } else {
+                    break;
+                }
+            }
+
+            if !is_duplicate {
+                current.next = Some(Box::new(ListNode::new(node.val)));
+                current = current.next.as_mut().unwrap();
+            }
+
+            head = node.next.take();
+        }
+        new_head.next
+    }
 }
