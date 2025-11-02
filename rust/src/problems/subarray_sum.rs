1e4eba53feb3f9ff5040aaf9f7797383c24f975b
RyoGreen <41415120ryo@gmail.com>
2025-11-03 00:00:31 +0900

    subarray sum

--- rust/src/problems/subarray_sum.rs
+++ rust/src/problems/subarray_sum.rs
@@ -2,6 +2,15 @@ pub struct Solution;
 
 impl Solution {
     pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
-        0
+        let mut result = 0;
+        for i in 0..nums.len() {
+            for j in i + 1..=nums.len() {
+                let sub = &nums[i..j];
+                if k == sub.iter().sum() {
+                    result += 1
+                }
+            }
+        }
+        result
     }
 }
