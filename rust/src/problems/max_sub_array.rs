7abd699e3b05943a64f65e51f1be84d4f51cb766
RyoGreen <41415120ryo@gmail.com>
2025-12-14 21:22:32 +0900

    max array sum

--- rust/src/problems/max_sub_array.rs
+++ rust/src/problems/max_sub_array.rs
@@ -1,6 +1,15 @@
 pub struct Solution;
 impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-        1
+        if nums.len() < 1 {
+            return 0;
+        }
+        let mut current_sum = nums[0];
+        let mut max_sum = nums[0];
+        for n in 1..nums.len() {
+            current_sum = current_sum.max(0) + nums[n];
+            max_sum = max_sum.max(current_sum);
+        }
+        max_sum
     }
 }
