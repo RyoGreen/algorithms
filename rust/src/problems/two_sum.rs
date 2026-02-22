a38232e7c6d588bf1507980d65992b02119abca7
RyoGreen <41415120ryo@gmail.com>
2026-02-23 01:12:45 +0900

    test two_sum

--- rust/src/problems/two_sum.rs
+++ rust/src/problems/two_sum.rs
@@ -11,3 +11,16 @@ impl Solution {
         vec![]
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_two_sum() {
+        let nums = vec![2, 7, 11, 15];
+        let target = 9;
+        let result = Solution::two_sum(nums, target);
+        assert_eq!(result, vec![0, 1]);
+    }
+}
