3d3c7f75c3476814937a9d07aaf6e45442a325bc
RyoGreen <41415120ryo@gmail.com>
2026-02-23 01:10:02 +0900

    test move_zeros

--- rust/src/problems/move_zeros.rs
+++ rust/src/problems/move_zeros.rs
@@ -17,3 +17,18 @@ impl Solution {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_move_zeros() {
+        let mut nums = vec![0, 1, 0, 3, 12];
+        Solution::move_zeros(&mut nums);
+        assert_eq!(nums, vec![1, 3, 12, 0, 0]);
+
+        let mut nums = vec![0, 0, 1];
+        Solution::move_zeros(&mut nums);
+        assert_eq!(nums, vec![1, 0, 0]);
+    }
+}
