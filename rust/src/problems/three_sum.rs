d3770e6a567a1a86c16d3489bfbacc6229da6004
RyoGreen <41415120ryo@gmail.com>
2026-02-23 01:11:24 +0900

    test three_sum

--- rust/src/problems/three_sum.rs
+++ rust/src/problems/three_sum.rs
@@ -39,3 +39,31 @@ impl Solution {
         result
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_max_profit() {
+        let nums = vec![-1, 0, 1, 2, -1, -4];
+        let expected = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
+        assert_eq!(Solution::three_sum(nums), expected);
+
+        let nums = vec![1, 2, 3];
+        let expected: Vec<Vec<i32>> = Vec::new();
+        assert_eq!(Solution::three_sum(nums), expected);
+
+        let nums: Vec<i32> = Vec::new();
+        let expected: Vec<Vec<i32>> = Vec::new();
+        assert_eq!(Solution::three_sum(nums), expected);
+
+        let nums = vec![-1, 0, 1, 2, -1, -4, 1, -1, -1, 0];
+        let expected = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
+        assert_eq!(Solution::three_sum(nums), expected);
+
+        let nums = vec![-1, 0, 1, 2, -1, -4];
+        let expected = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
+        assert_eq!(Solution::three_sum(nums), expected);
+    }
+}
