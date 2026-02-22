5eeab77b18a3ce56cf2bfc459814f897be60c779
RyoGreen <41415120ryo@gmail.com>
2026-02-23 01:09:34 +0900

    test max_sub_array

--- rust/src/problems/max_sub_array.rs
+++ rust/src/problems/max_sub_array.rs
@@ -13,3 +13,27 @@ impl Solution {
         max_sum
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_max_sub_array() {
+        assert_eq!(
+            Solution::max_sub_array(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]),
+            6
+        );
+
+        assert_eq!(Solution::max_sub_array(vec![5]), 5);
+
+        assert_eq!(Solution::max_sub_array(vec![-5]), -5);
+
+        assert_eq!(Solution::max_sub_array(vec![-8, -3, -6, -2, -5, -4]), -2);
+
+        assert_eq!(Solution::max_sub_array(vec![1, 2, 3, 4]), 10);
+
+        assert_eq!(Solution::max_sub_array(vec![4, -10, 3, 5]), 8);
+
+        assert_eq!(Solution::max_sub_array(vec![]), 0);
+    }
+}
